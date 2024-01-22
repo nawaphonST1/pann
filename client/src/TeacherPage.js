734dcdcec881e0ff7787a42dc60223bb334ea48c
Nawaphon <namaza545@gmail.com>
2024-01-23 01:54:56 +0700

    TeacherPage add event

--- client/src/TeacherPage.js
+++ client/src/TeacherPage.js
@@ -1,13 +1,28 @@
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
+import React from 'react';
+import Eventform from './component/event';
+import axios from 'axios';
 
 function TeacherPage() {
   return (
     <div class="container">
       Teacher Page
+      <h1>Announcement of event</h1>
+      <Eventform />
     </div>
     
   );
+
 }
+
+// const TeacherPage = () => {
+//   return (
+//     <div>
+//       <h1>Activity Tracker</h1>
+//       <Eventform />
+//     </div>
+//   );
+// };
 
 export default TeacherPage;
