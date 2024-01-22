4ec02035171d4beefcc592868e02ab66bef2fc8b
Nawaphon <namaza545@gmail.com>
2024-01-23 02:22:47 +0700

    eventstudent to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
 import LoginForm from './LoginForm';
+import React from 'react';
+import Eventform from './component/event';
+import { useState } from 'react';
 
 function App() {
   const clickMe = () => console.log('hello world')
+  const [authenticated, setAuthenticated] = useState(false);
   return (
     <div class="container">
-      <LoginForm/>
+      <LoginForm setAuthenticated={setAuthenticated} />
     </div>
   );
 }
